bded044f454ad61abb5626986ffd5cbd3190dc1e
ADT2812 <adinetra36@gmail.com>
2026-07-08 23:53:41 +0530

    user page and admin page open successfully.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import {setLocalStorage} from './utils/localStorage'
-import { useEffect } from 'react'
+import { setLocalStorage } from './utils/localStorage'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
-  useEffect(() => {
-    setLocalStorage()
+  const [user, setUser] = useState(null)
 
-  },)
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      
+    } else if (email == 'user@me.com' && password == '123') {
+      setUser('employee')
+      
+    } else {
+      alert('Invalid credentials')
+    }
+  }
+
+  const data=useContext(AuthContext)
+  console.log(data)
 
   return (
     <>
-      <Login />
-      {/* <EmployeeDashboard />
-      <AdminDashboard />  */}
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+
+      {user == 'admin' ? (
+        <AdminDashboard />
+      ) : (
+        <EmployeeDashboard />
+      )}
     </>
   )
 }
